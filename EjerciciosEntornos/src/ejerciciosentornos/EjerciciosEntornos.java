d30da30cf58a68ea078b8f7f194b75cb1b25a831
fersan <fersan@ubuntu>
2021-12-02 18:50:38 +0100

    Modificacions

--- EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
+++ EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
@@ -6,12 +6,16 @@ public class EjerciciosEntornos {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        float max = sc.nextFloat();
         System.out.println("Buenos Dias!!");
+        
 
-        for(int cont = 0; cont<5; cont++) {
-           
-            System.out.println("Pide la temperatura de el municiio");
+        for(int cont = 0; cont<max; cont++) {
+            
+            System.out.println("Pide la temperatura de el municipio");
             float temp = sc.nextFloat();
+            
+            
                    if (temp < 0) {
             System.out.println("La temperatura esta en negativo");
         } else {
