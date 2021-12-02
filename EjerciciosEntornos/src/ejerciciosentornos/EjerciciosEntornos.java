e92afadf58a3c75dc3b276385bd99d3d6be70479
fersan <fersan@ubuntu>
2021-12-02 19:07:00 +0100

    Modificacions

--- EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
+++ EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
@@ -8,7 +8,10 @@ public class EjerciciosEntornos {
         
         
         System.out.println("Buenos Dias!!");
-        
+        int mayor = 0;
+        int menor = 0;
+        int mayorS = 0;
+        int menorS = 0;
         System.out.println("Ingrese el numero de los municipios: ");
         Scanner sc = new Scanner(System.in);
         int max = sc.nextInt();
@@ -18,15 +21,29 @@ public class EjerciciosEntornos {
             float temp = sca.nextFloat();
             
             
-                   if (temp < 0) {
+        if (temp < 0) {
             System.out.println("La temperatura esta en negativo");
+            System.out.println("La temperatura mayor de los casos pedidos es" +temp);
+            System.out.println("La temperatura menor de los casos pedidos es" +temp);
+            
         } else {
             System.out.println("La temperatura esta en positivo");
+            
+        
+ 
+ 
+        if (temp<mayor) {
+                       temp = mayor;
+        } else {
+                temp = menor;
+                }
         }
+                   
+        
+        System.out.println("La temperatura mayor es"  +mayor  + "y la temperatura minima es " +menor);
+        
         }
-        
-        
-        
     }
-
 }
+
+
