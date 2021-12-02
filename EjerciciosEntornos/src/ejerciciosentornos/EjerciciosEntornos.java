5b19c41b9df81bf99e764f65784aec2339653110
fersan <fersan@ubuntu>
2021-12-02 17:17:40 +0100

    Modificacions

---
+++ EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
@@ -0,0 +1,11 @@
+
+package ejerciciosentornos;
+
+public class EjerciciosEntornos {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Buenos dias!!");
+    }
+    
+}
