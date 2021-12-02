63942324ada09500e6ec561acee35c2638e0433d
fersan <fersan@ubuntu>
2021-12-02 17:32:41 +0100

    Modificacions

--- EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
+++ EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
@@ -1,11 +1,21 @@
-
 package ejerciciosentornos;
+
+import java.util.Scanner;
 
 public class EjerciciosEntornos {
 
-   
     public static void main(String[] args) {
-        System.out.println("Buenos dias!!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la temperatura que hace");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println("La temperatura esta en negativo");
+        } else {
+            System.out.println("La temperatura esta en positivo");
+        }
+
     }
-    
+
 }
