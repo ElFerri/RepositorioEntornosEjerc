a84ac28719436f0196b4fad9a3ef063827904c03
fersan <fersan@ubuntu>
2021-12-16 18:52:15 +0100

    Modificacions

--- EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
+++ EjerciciosEntornos/src/ejerciciosentornos/EjerciciosEntornos.java
@@ -9,25 +9,24 @@ public class EjerciciosEntornos {
 
         double mayor = Double.MIN_VALUE;
         double menor = Double.MAX_VALUE;
-        
+
         double[] temperaturas;
-        
+
         Scanner sc = new Scanner(System.in);
         float temp;
-        
-//ey
+
         System.out.println("Buenos Dias!!");
         System.out.println("Ingrese el numero de los municipios: ");
 
         int max = sc.nextInt();
-        //para guardar los numeros en un array
+
         temperaturas = new double[max];
 
         for (int cont = 0; cont < max; cont++) {
 
             System.out.println("Dime la temperatura del municipio");
             temp = sc.nextFloat();
-            
+            temperaturas[cont] = temp;
 
             if (temp < 0) {
                 System.out.println("La temperatura esta en negativo");
@@ -36,11 +35,11 @@ public class EjerciciosEntornos {
                 System.out.println("La temperatura esta en positivo");
 
             }
-            
+
             if (temp > mayor) {
                 mayor = temp;
             }
-            
+
             if (temp < menor) {
                 menor = temp;
             }
@@ -48,6 +47,11 @@ public class EjerciciosEntornos {
 
         System.out.println("La temperatura mayor es " + mayor + " y la temperatura minima es " + menor);
 
+        System.out.println("Las temperaturas han sido guardadas y son las siguientes");
+        for (int i = 0; i < temperaturas.length; i++) {
+            System.out.println(temperaturas[i]);
+
+        }
+
     }
 }
-
